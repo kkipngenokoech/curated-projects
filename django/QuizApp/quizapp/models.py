79eb83b5f5a26a378e86a729a655eca9b0916a7b
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-10-23 22:29:31 +0300

    created a new model database

--- django/QuizApp/quizapp/models.py
+++ django/QuizApp/quizapp/models.py
@@ -1,3 +1,14 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class QuestionModel(models.Model):
+    question = models.CharField(max_length=200,nul=True)
+    option_one = models.CharField(max_length=200,nul=True)
+    option_two = models.CharField(max_length=200,nul=True)
+    option_three = models.CharField(max_length = 200,nul = True)
+    option_four = models.CharField(max_length = 200,nul = True)
+    answer = models.CharField(max_length = 200,nul = True)
+    def __str__ (self):
+        return self.question
+        
