090eb58694915670842127f8bc972bb40e37894b
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-10-27 10:33:05 +0300

    adding git ignore

--- javascript/WeatherApp/index.js
+++ javascript/WeatherApp/index.js
@@ -1,7 +1,22 @@
 console.log("hey")
-const form = document.getElementById('city')
 const answer = document.getElementById('paste-answer')
-const data = fetch('https://api.openweathermap.org/data/2.5/weather?q=portland&appid=[]', {mode: 'cors'})
-.then(function(response){
-    answer.innerHTML = response.json()
+const city = "cairo"
+const submit = document.getElementById('button-submit')
+submit.addEventListener('click', function (){
+    const inputCity = document.getElementById('city').value
+    console.log(inputCity)
 })
+// const data = fetch('https://api.openweathermap.org/data/2.5/weather?q=portland&appid=[]', {mode: 'cors'})
+// .then(function(response){
+//     answer.innerHTML = response.json()
+// })
+
+const dataInputs = async()=>{
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=API_KEY`);
+    console.log(response);
+        const responseData = await response.json();
+        console.log(responseData);
+        answer.innerHTML = response
+    }
+    
+    dataInputs();
