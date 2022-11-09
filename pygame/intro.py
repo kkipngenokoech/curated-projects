4f6bc47cee6481410e60e9d46a33ab5e3e575617
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-11-09 11:53:04 +0300

    pygame drawing screen and rectangle

--- pygame/intro.py
+++ pygame/intro.py
@@ -1,3 +1,25 @@
 import pygame
-successes, failures = pygame.init()
-print("{} successes and {} failures".format(successes, failures))
+# successes, failures = pygame.init()
+# print("{} successes and {} failures".format(successes, failures))
+
+# screen = pygame.display.set_mode((720,480))
+
+# pygame.display.set_caption('Name')
+# pygame.display.update()
+background_color = (255,255,255)
+(width, height) = (1000, 2000) # screen size
+color = ( 0, 0, 0) # for rectangle
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Drawing Rectangle')
+screen.fill(background_color)
+pygame.draw.rect(screen, color, (100,50, 30, 40), 1)
+pygame.display.update()
+
+# loop
+running = True
+while(running):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+
