23e0835ff5f621c23b763351821415c755b825ab
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-11-18 00:56:22 +0300

    drawing a dawg

---
+++ go/gopherit/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("")
+	fmt.Println(" __      _")
+	fmt.Println("o'')}___//")
+	fmt.Println(" `_/      )")
+	fmt.Println(" (_(_/-(_/ ")
+}
