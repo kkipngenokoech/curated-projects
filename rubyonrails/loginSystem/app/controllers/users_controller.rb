fb413a0c983b7123f60796c0b5da1562d961eed5
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-11-18 19:18:59 +0300

    controller and forms

--- rubyonrails/loginSystem/app/controllers/users_controller.rb
+++ rubyonrails/loginSystem/app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_path, notice: "please check your email for confirmation details"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def new
+        @user = User.new
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
