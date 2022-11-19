58a2c0f4a3dfa671ffb7d5d6b3f659c0c0596195
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-11-19 20:11:16 +0300

    marvel

---
+++ rubyonrails/marvel/app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+
+end
---
+++ rubyonrails/marvel/config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  #get 'welcome/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #resources :welcome
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "welcome#index"
+  get "signup", to: "registrations#new"
+  post "signup", to: "registrations#create"
+  get "signin", to: "sessions#new"
+  post "signin", to: "sessions#create", as: "login"
+  get "logout", to: "sessions#destroy"
+  get "password", to: "passwords#edit", as: "editpassword"
+  patch "password", to: "passwords#update"
+  get "password/reset", to: "passwords_resets#new"
+  post "password/reset", to: "passwords_resets#create"
+  get "password/reset/edit", to: "passwords_resets#edit"
+  patch "password/reset/edit", to: "passwords_resets#update"
+end
