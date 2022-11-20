0f82bd97652ed12855e22363795f6484e33de7bb
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-11-20 22:51:12 +0300

    working on the delete functionality

--- rubyonrails/marvel/app/controllers/tshirt_controller.rb
+++ rubyonrails/marvel/app/controllers/tshirt_controller.rb
@@ -1,11 +1,19 @@
 class TshirtController < ApplicationController
   def create
+    
   end
 
   def store
     @image = Cloudinary::Uploader.upload(params[:media])
     @tshirt = Tshirt.new({:color => params[:color], :size => params[:size],:character => params[:character], :img_url => @image['secure_url']})
     @tshirt.save
+    redirect_to ('/')
+  end
+
+  def destroy
+    @tshirt = Tshirt.find(params[:id])
+    @tshirt.destroy
+    flash[:notice] = "the t-shirt was successfully destroyed"
     redirect_to ('/')
   end
 end
--- rubyonrails/marvel/config/routes.rb
+++ rubyonrails/marvel/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'tshirt/create'
   post 'tshirt/store'
+  delete 'deletetshirt/:id', to: "tshirt#destroy"
   #get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #resources :welcome
