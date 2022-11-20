b6b8250bc218c27516d4dff9de733ea79ebbfe65
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-11-20 20:31:13 +0300

    tshirt controller

--- rubyonrails/marvel/app/controllers/tshirt_controller.rb
+++ rubyonrails/marvel/app/controllers/tshirt_controller.rb
@@ -3,5 +3,8 @@ class TshirtController < ApplicationController
   end
 
   def store
+    @tshirt = Tshirt.new({:color => params[:color], :size => params[:size],:character => params[:character]})
+    @tshirt.save
+    redirect_to ('/')
   end
 end
--- rubyonrails/marvel/config/routes.rb
+++ rubyonrails/marvel/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'tshirt/create'
-  get 'tshirt/store'
+  post 'tshirt/store'
   #get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #resources :welcome
