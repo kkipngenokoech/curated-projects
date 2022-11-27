b3e84794709ba1691ffcf422fa4d1a3baa19ce16
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-11-27 07:52:29 +0300

    devise

--- rubyonrails/marvel/app/controllers/application_controller.rb
+++ rubyonrails/marvel/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    # before_action :set_current_user
+    before_action :set_current_user
     # def set_current_user
     #     Current.user = User.find_by(id = session[:user_id]) if session[:user_id]
     # end
--- rubyonrails/marvel/app/models/user.rb
+++ rubyonrails/marvel/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  #before_action :authenticate_user!
+    #before_action :authenticate_user!
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
