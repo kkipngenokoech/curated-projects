b8ce7df1e4cc7a05ef08433d4ec009770d45ecea
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-12-02 07:58:43 +0300

    adding the dewise buttons

--- rubyonrails/micro-reddit/db/schema.rb
+++ rubyonrails/micro-reddit/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_02_040642) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_043848) do
   create_table "comments", force: :cascade do |t|
     t.string "author"
     t.text "comment"
@@ -37,7 +37,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_02_040642) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
