53e5e45dd14354e2ea24a64b53fac32eeac2d30c
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-12-03 10:41:38 +0300

    the edit button is working"

--- rubyonrails/devise/marvel/app/controllers/tshirt_controller.rb
+++ rubyonrails/devise/marvel/app/controllers/tshirt_controller.rb
@@ -1,6 +1,6 @@
 class TshirtController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :filter]
-  before_action :authenticate_admin!, only: [:new]
+  before_action :authenticate_admin!, only: [:new,:edit,:delete]
   def index
     @tshirts = Tshirt.where(nil)
     @tshirts = Tshirt.filter_by_color(params[:color]) if params[:color].present?
@@ -12,7 +12,15 @@ class TshirtController < ApplicationController
   end
   def create   
   end
-
+  def edit
+    @tshirt = Tshirt.find(params[:id])
+  end
+  def update
+    @tshirt = Tshirt.find(params[:id])
+    @tshirt.update(tshirt_params) 
+    #render json: {status:"success"}
+    redirect_to root_path
+  end
   def show
     if Tshirt.where(params[:id]) === nil
       flash[:notice] = "TShirt not found"
@@ -43,5 +51,10 @@ class TshirtController < ApplicationController
 
   def coord
     @tshirts = Tshirt.all
+  end
+
+  private
+  def tshirt_params
+    params.require(:tshirt).permit(:color, :size, :character) 
   end
 end
