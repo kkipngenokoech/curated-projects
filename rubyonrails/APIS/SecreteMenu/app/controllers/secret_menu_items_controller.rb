f21dc21cace8b227e5e07c5871c71cd1c162f397
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-12-18 15:23:18 +0300

    setting up the routes in the controller

--- rubyonrails/APIS/SecreteMenu/app/controllers/secret_menu_items_controller.rb
+++ rubyonrails/APIS/SecreteMenu/app/controllers/secret_menu_items_controller.rb
@@ -1,2 +1,35 @@
 class SecretMenuItemsController < ApplicationController
+    def index
+        @secret_menu_items = SecretMenuItem.all
+        render json: @secret_menu_items
+    end
+    
+    def show
+        @secret_menu_item = SecretMenuItem.find(params[:id])
+        render json: @secret_menu_item
+    end
+    def create
+        @secret_menu_item = SecretMenuItem.create(
+            menu_name: params[:menu_name],
+            restaurant_name: params[:restaurant_name],
+            menu_description: params[:menu_description]
+        )
+        render json: @secret_menu_item
+    end
+    def update
+        @secret_menu_item = SecretMenuItem.find(params[:id])
+        @secret_menu_item.update(
+            menu_name: params[:menu_name],
+            restaurant_name: params[:restaurant_name],
+            menu_description: params[:menu_description]
+        )
+        render json: @secret_menu_item
+    end
+    def destroy
+        @secret_menu_items = SecretMenuItem.all
+        @secret_menu_item = SecretMenuItem.find(params[:id])
+        @secret_menu_item.destroy
+        render json: @secret_menu_item
+    end
 end
+
