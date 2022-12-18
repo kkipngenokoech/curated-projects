7d8f43ad7e35ccea658138e3f9e370c1d9d0c0cc
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-12-18 15:09:31 +0300

    secret menu schema

--- rubyonrails/APIS/SecreteMenu/db/migrate/20221218120146_create_secret_menu_items.rb
+++ rubyonrails/APIS/SecreteMenu/db/migrate/20221218120146_create_secret_menu_items.rb
@@ -1,7 +1,9 @@
 class CreateSecretMenuItems < ActiveRecord::Migration[7.0]
   def change
     create_table :secret_menu_items do |t|
-
+      t.string :menu_name
+      t.string :restaurant_name
+      t.string :menu_description
       t.timestamps
     end
   end
