c8866fba96938b14de0f5a28167261bd3c2ef024
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-12-18 15:23:42 +0300

    setting up the routes allowed actions

--- rubyonrails/APIS/SecreteMenu/config/routes.rb
+++ rubyonrails/APIS/SecreteMenu/config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  resources :secret_menu_items
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :secret_menu_items, only: [:index, :show, :create, :update, :destroy]
 end
--- rubyonrails/APIS/SecreteMenu/db/seeds.rb
+++ rubyonrails/APIS/SecreteMenu/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+menu1 = SecretMenuItem.create(menu_name: "Chipotle Nachos", restaurant_name: "Chipotle", menu_description:"Build a plate of nachos with all of your favorite fixings")
+menu2 = SecretMenuItem.create(menu_name: "Starbucks butterbeer Frappuccino", restaurant_name: "Starbucks", menu_description:"Combine three pumps of toffee nut syrup and three pumps of caramel with a Crème Frappuccino base")
+menu3 = SecretMenuItem.create(menu_name: "Skittles", restaurant_name: "Jamba Juice", menu_description:"A mixture of lemonade, lime sherbet, frozen yogurt, and strawberries")
