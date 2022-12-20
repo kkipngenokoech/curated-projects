559344a00d9cd7f06d0218f3bcb058c9be35618a
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-12-20 09:47:07 +0300

    my form is working

--- rubyonrails/railsgirls/app/controllers/forms_controller.rb
+++ rubyonrails/railsgirls/app/controllers/forms_controller.rb
@@ -4,6 +4,12 @@ class FormsController < ApplicationController
     def new
     end
     def create
+        redirect_to root_path, notice: "form submitted successfully"
+    end
+    def search
+        
         redirect_to root_path
+    end
+    def show
     end
 end
